82cdf25c25c317015c918e1d1039d7c68e83cf18
Nurbolot Muratbekuulu <savagenurkg@gmail.com>
2024-02-04 14:23:52 +0600

    second commit

---
+++ lib/1 copy.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NamePage extends StatefulWidget {
+  const NamePage({super.key});
+
+  @override
+  State<NamePage> createState() => _NamePageState();
+}
+
+class _NamePageState extends State<NamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
